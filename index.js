3a2f1c6740c2892f26de25470774c8a851a67857
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-03 12:06:08 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if((number)<=400){
+    return 'This one is on me!'
+  } else if ((number)>2000 &&(number)<=2500 ){
+  return 'I will gladly take your thirty bucks.'}
+  else return'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(string){
+  return string==="NYC"?"Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(string){
+  switch (string) {
+  case "generous":
+  return  "Thank you so much."
+case "not as generous":
+return 'Thank you.'
+default:
+  return 'Bye.'
 }
+  
+    
+  }
